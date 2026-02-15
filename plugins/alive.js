5a1d16b47a2851c727b9d68b00e0a2bde43314a1
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2026-02-15 23:55:35 +0500

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -53,8 +53,8 @@ module.exports = {
           forwardingScore: 999,
           isForwarded: true,
           forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363319098372999@newsletter',
-            newsletterName: 'MEGA MD',
+            newsletterJid: '120363425143124298@newsletter',
+            newsletterName: '𝐅𝐀𝐈𝐙𝐀𝐍-𝐌𝐃',
             serverMessageId: -1
           }
         }
